17a8fe5a261dbead6950afcd1cb6d082854716e2
simplyratl <nikicaraznatovic123@gmail.com>
2024-08-11 21:10:24 +0200

    dodavanje tailwinda

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,11 @@
 import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
 import { KcPage } from "./kc.gen";
+import "./styles.css";
 
 // The following block can be uncommented to test a specific page with `yarn dev`
 // Don't forget to comment back or your bundle size will increase
-/*
+
 import { getKcContextMock } from "./login/KcPageStory";
 
 if (import.meta.env.DEV) {
@@ -14,7 +15,7 @@ if (import.meta.env.DEV) {
         overrides: {}
     });
 }
-*/
+
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
